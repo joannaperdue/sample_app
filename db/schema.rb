a3f44b8c7cabe19833935c5e9808b4dd73f4485b
Joanna Perdue <joanna.perdue@verizon.net>
2018-04-24 19:08:19 -0400

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_215810) do
+ActiveRecord::Schema.define(version: 2018_04_24_225527) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_04_24_215810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.decimal "price"
   end
 
 end

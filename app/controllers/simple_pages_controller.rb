7f238d8e5416b67fee0f6be4ad13c1a3704ebe1b
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-08 21:21:04 -0400

    fix syntax error in user_mailer

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -12,11 +12,5 @@ class SimplePagesController < ApplicationController
       to: 'joanna.perdue@verizon.net',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
-  end
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,4 @@ class UserMailer < ApplicationMailer
     mail(from: email,
          to: 'your-email@example.com',
          subject: "A new contact form message from #{name}")
-  end
 end

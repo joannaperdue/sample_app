3939b328a219d7b33038b567969b34ac2a4585f7
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-10 21:32:12 -0400

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: ‘catsareevil.herokuapp.com’ }
+  config.action_mailer.default_url_options = { host: ‘bikeberlincentral.herokuapp.com’ }
 end

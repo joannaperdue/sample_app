a5ba754070edda037855ecd19eec2f59aa3f7d40
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-16 22:03:56 -0400

    product view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-
+  has_many :comments
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
   def self.search(search_term)
     if Rails.env.development?
       like_operator = "LIKE"

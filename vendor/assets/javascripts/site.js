573b5b7e6e8e20ee6eda4f7307a9a16bab6ec341
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-17 17:05:46 -0400

    update raty

--- vendor/assets/javascripts/site.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function(){
-  $('.rating').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
-    starOn: '<%= asset_path 'star-on.png' %>'
-    });
-});

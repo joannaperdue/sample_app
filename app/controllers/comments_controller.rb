4da1e858f2ae60ad0abad5cf815d413d1cc7cecf
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-17 15:05:48 -0400

    render partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,20 @@
 class CommentsController < ApplicationController
-  def create
-  @product = Product.find(params[:product_id])
-  @comment = @product.comments.new(comment_params)
-  @comment.user = current_user
-  @comment.save
-  redirect_to product_path(@product)
-end
 
-def destroy
-   @comment = Comment.find(params[:id])
-   product = @comment.product
-   @comment.destroy
-   redirect_to product
- end
+	def create
+		@product = Product.find(params[:product_id])
+		@comment = @product.comments.new(comment_params)
+		@comment.user = current_user
+		@comment.save
+		redirect_to product_path(@product)
+	end
 
-  private
+	def destroy
+	end
 
-    def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating)
-    end
+	private
+
+	  def comment_params
+	    params.require(:comment).permit(:user_id, :body, :rating)
+	  end
 
 end

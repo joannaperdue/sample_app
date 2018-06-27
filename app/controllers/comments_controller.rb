4a977f1f781ccd49ff4496d96ce1dcdc10c67e2b
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-27 19:02:28 -0400

    add paginate to index and show methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
 	      format.json { render json: @comment.errors, status: :unprocessable_entity }
 	    end
 	  end
+	end
 
 	def destroy
 	end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,19 +4,20 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = @products.paginate(:page => params[:page], :per_page=>3)
     if params[:q]
       search_term = params[:q]
       @products = Product.search(search_term)
     else
       @products = Product.all
     end
+    @products = @products.paginate(:page => params[:page], :per_page=>3)
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
+    @comments = @comments.paginate(page: params[:page], per_page: 3)
   end
 
   # GET /products/new

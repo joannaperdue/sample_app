fd5f84d00a069061cc7ea2eec5d022cdb6592310
Joanna Perdue <joanna.perdue@verizon.net>
2018-06-29 18:29:52 -0400

    set primary user account to admin

--- db/migrate/20180628204757_add_admin_flag_to_users.rb
+++ db/migrate/20180628204757_add_admin_flag_to_users.rb
@@ -1,4 +1,4 @@
-class AddAdminFlagToUsers < ActiveRecord::Migration
+class AddAdminFlagToUsers <  ActiveRecord::Migration[5.2]
   def change
     add_column :users, :admin, :boolean, default: false, null: false
   end

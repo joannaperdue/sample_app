fd9ecfe1d824a31bccfb7ec5ac9339a9a54a0ed7
Joanna Perdue <joanna.perdue@verizon.net>
2018-07-01 20:20:49 -0400

    attempt to add user controller test

--- Gemfile
+++ Gemfile
@@ -77,3 +77,4 @@ end
 gem 'devise'
 gem 'cancancan', '~> 1.10'
 gem 'will_paginate', '~> 3.1.1'
+gem 'rails-controller-testing'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+
+  @user1=let(:user) { User.create!(email: "joannap@yahoo.com",password: "Testpass456") }
+  @user2=let(:user) { User.create!(email: "testuser@yahoo.com",password: "Testuser") }
+
+  describe 'GET #show' do
+    context 'when a user is logged in' do
+
+      before do
+        sign_in user
+      end
+      it 'loads correct user details' do
+        get :show, params: { id: @user.id }
+        expect(assigns(:user)).to eq @user
+        expect(response).to have_http_status(200)
+      end
+      it 'cant access other users show page' do
+        get :show, params: { id: @user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when a user is not logged in' do
+      it 'redirects to login' do
+        get :show, params: { id: user.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end

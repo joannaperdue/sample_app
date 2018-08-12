45ff74ea5b11b448270ab6bc47ea1185d537ab5f
Joanna Perdue <joanna.perdue@verizon.net>
2018-08-12 16:23:39 -0400

    current password on form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :orders
-    validates :password, :on => :create
 end

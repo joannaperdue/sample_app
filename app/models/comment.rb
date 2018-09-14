0e88eeeb4c1391d438b3dcb0b0545033eba702ba
Joanna Perdue <joanna.perdue@verizon.net>
2018-09-14 14:23:58 -0400

    ActiveJob

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
+
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
 end

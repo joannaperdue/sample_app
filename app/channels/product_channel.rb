e7de89d0d8a2091271fb5f2b5f219b936b0cd297
Joanna Perdue <joanna.perdue@verizon.net>
2018-09-14 14:06:14 -0400

    authentication for connection

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,10 +1,14 @@
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "product_channel"
+    # stream_from "product_channel"
     # stream_from "some_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+  def listen
+    stop_all_streams (data)
+    stream_for data["product_id"]
   end
 end
